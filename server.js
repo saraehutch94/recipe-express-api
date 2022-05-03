af8c97663eca370185d6795c9f10f459d7c986fe
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-05-03 13:17:29 -0400

    test index route on localhost / install method override middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Require dependencies
 const express = require("express");
+const methodOverride = require("method-override");
 
 // Initialize Express application
 const app = express();
@@ -11,6 +12,14 @@ require("dotenv").config();
 
 // Save port value to variable
 const PORT = process.env.PORT;
+
+// Mount middleware
+app.use(methodOverride("_method"));
+
+// Define routes
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 // Tell Express app to listen for client requests
 app.listen(PORT, () => {
