2405c6e10b442da56cee2a717628934fb2abb16d
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-05-03 13:12:38 -0400

    add dotenv dependency / set up port value

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
+// Require dependencies
 const express = require("express");
 
+// Initialize Express application
+const app = express();
+
+// Configure application settings
+
+// Set up port value
+require("dotenv").config();
+
+// Save port value to variable
+const PORT = process.env.PORT;
